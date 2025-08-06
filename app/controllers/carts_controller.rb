8ec6fae968733d1307c7cc885011f5c57c12f083
dk1375108@gamil.com <your-email@example.com>
2025-08-06 14:32:45 +0530

    change for cart show page

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,7 +2,7 @@ class CartsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @cart = current_user.cart
+    @cart = Cart.find_or_create_by(user_id: current_user.id)
     @cart_items = @cart.cart_items.includes(:menu_item)
   end
 end
