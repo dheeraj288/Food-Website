fd9400b2996b721aced391131ce5aba6e16f751e
dk1375108@gamil.com <your-email@example.com>
2025-08-26 13:24:15 +0530

    revert changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,14 +94,4 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'gmail.com',
-    user_name: 'dheerajkalwar866@gmail.com',
-    password: 'xzvj gcrb kdlj mvvz',
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
 end
